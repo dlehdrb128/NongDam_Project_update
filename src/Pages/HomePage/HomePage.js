c0835302485e387c49b72ad59b32639b18bd6e42
Yehoonn <jes6208@naver.com>
2022-10-25 16:43:20 +0900

    'update'

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,9 @@
-
 import styled from "styled-components";
 import SetSidemenu from "../../common/SetSidemenu";
 import SetMypageTop from "../../common/SetMypageTop";
 import SetMypageList from "../../common/SetMypageList";
-import ProductItem from "../ProductPage/ProductItem";
-import SellerInfo from '../../common/SellerInfo';
+
+import SellerInfo from "../../common/SellerInfo";
 
 // 홈페이지
 const MainBox = styled.main`
@@ -27,10 +26,9 @@ const HomePage = () => {
         <SetMypageTop />
         <SetSidemenu Text={"그냥만듬"}></SetSidemenu>
         <SetMypageList></SetMypageList>
-        <SetSidemenu Text={'상호'}></SetSidemenu>
-        <SetMypageList Text={'최근주문정보'}></SetMypageList>
+        <SetSidemenu Text={"상호"}></SetSidemenu>
+        <SetMypageList Text={"최근주문정보"}></SetMypageList>
         <SellerInfo></SellerInfo>
-        <ProductItem></ProductItem>
       </div>
     </>
   );

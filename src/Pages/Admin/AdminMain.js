0f3e0b44f75f751a00facab73704e588aef1d349
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-10-26 19:16:32 +0900

    admin 메인페이지 수정

--- src/Pages/Admin/AdminMain.js
+++ src/Pages/Admin/AdminMain.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import AdminBox from '../../common/AdminBox';
+import AdminBox from './AdminBox';
 import { Link } from 'react-router-dom';
 
-
-// 전체 감싼 메인 박스
+// 전체 감싼 메인 박스 ( 전체화면 )
 const MainBox = styled.div`
   width: inherit;
   padding-top: 145px;
@@ -23,7 +22,7 @@ const MainBox = styled.div`
     color: ${({ theme }) => theme.lightblack};
   }
 
-  // 스토어관리 박스들 한번에 묶은
+  // 스토어관리 박스들 한번에 묶은 div
   & > div {
     width: 950px;
     height: 550px;
@@ -32,15 +31,87 @@ const MainBox = styled.div`
     justify-content: space-between;
     align-content: space-between;
     padding-top: 70px;
+    & > a {
+      &:hover {
+        font-weight: bold;
+        cursor: pointer;
+        color: ${({ theme }) => theme.orange};
+        & > div {
+          & > span {
+            color: ${({ theme }) => theme.orange};
+          }
+        }
+      }
+    }
+    & > div {
+      &:hover {
+        font-weight: bold;
+        cursor: pointer;
+        & > span {
+          color: ${({ theme }) => theme.orange};
+        }
+      }
+    }
   }
 `;
 
 const AdminMain = () => {
+  const componentData = [
+    {
+      link: 'create',
+      src: 'http://localhost:8080/icon/Admin_icon1.png',
+      name: '스토어개설',
+      content: '기본 스토어 개설',
+    },
+    {
+      link: 'store',
+      src: 'http://localhost:8080/icon/Admin_icon2.png',
+      name: '스토어관리',
+      content: '기본 스토어 관리',
+    },
+    {
+      link: 'newproduct',
+      src: 'http://localhost:8080/icon/Admin_icon3.png',
+      name: '상품관리',
+      content: '상품 등록, 수정',
+    },
+    {
+      link: 'board',
+      src: 'http://localhost:8080/icon/Admin_icon4.png',
+      name: '게시판관리',
+      content: '공지사항, 후기 관련',
+    },
+    {
+      link: 'money',
+      src: 'http://localhost:8080/icon/Admin_icon5.png',
+      name: '정산',
+      content: '판매현황, 금액 관리',
+    },
+    {
+      link: 'mypage',
+      src: 'http://localhost:8080/icon/Admin_icon6.png',
+      name: '마이페이지',
+      content: '나의 회원정보 수정',
+    },
+  ];
+
   return (
     <MainBox>
       <h1>스토어관리</h1>
       <div>
-        <Link to='create'>
+        {componentData.map((value, index) => {
+          return (
+            <Link to={`${value.link}`}>
+              <AdminBox
+                src={`${value.src}`}
+                name={`${value.name}`}
+                content={`${value.content}`}
+              ></AdminBox>
+            </Link>
+          );
+        })}
+
+        {/* <Link to='create'>
           <AdminBox
             src='http://localhost:8080/icon/Admin_icon1.png'
             name='스토어개설'
@@ -73,7 +144,7 @@ const AdminMain = () => {
           src='http://localhost:8080/icon/Admin_icon6.png'
           name='마이페이지'
           content='나의 회원정보 수정'
-        ></AdminBox>
+        ></AdminBox>*/}
       </div>
     </MainBox>
   );
